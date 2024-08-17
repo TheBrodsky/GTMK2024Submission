106cbe839f668f09af1a399f48ab1b88daf44f8a
TheBrodsky <stephensbrod@gmail.com>
2024-08-17 12:49:26 -0700

    added evolution menu, cleaned up player script more

---
+++ UI/evolutions_menu/evolutions_menu.gd
@@ -0,0 +1,21 @@
+extends TabContainer
+class_name EvolutionsMenu
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("open_evo_menu"):
+		visible = not visible # toggle visibility
+
+
+func _submit_changes() -> void:
+	var evo_tab: EvoPathTab = get_current_tab_control()
+	var skill_toggles: Array[bool] = evo_tab.get_skill_toggles()
+	Evolutions.update_evolution(current_tab, skill_toggles[0], skill_toggles[1])
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
+		_submit_changes()
--- util/evolutions.gd
+++ util/evolutions.gd
@@ -6,13 +6,40 @@ enum EvolutionPaths {WINGS, BRAIN, CLAWS}
 @export var path: EvolutionPaths = EvolutionPaths.WINGS
 
 # Wings
-var has_double_jump: bool = false
-var has_dash: bool = false
+var has_double_jump: bool = true:
+	get:
+		return has_double_jump and path == EvolutionPaths.WINGS
+var has_dash: bool = false:
+	get:
+		return has_dash and path == EvolutionPaths.WINGS
 
 # Brain
-var has_slo_mo: bool = false
-var has_rewind: bool = false
+var has_slo_mo: bool = false:
+	get:
+		return has_slo_mo and path == EvolutionPaths.BRAIN
+var has_rewind: bool = false:
+	get:
+		return has_rewind and path == EvolutionPaths.BRAIN
 
 # Claws
-var has_climb: bool = false
-var has_destroy_obstacles: bool = false
+var has_climb: bool = false:
+	get:
+		return has_climb and path == EvolutionPaths.CLAWS
+var has_destroy_obstacles: bool = false:
+	get:
+		return has_destroy_obstacles and path == EvolutionPaths.CLAWS
+
+
+func update_evolution(path: EvolutionPaths, has_first_skill: bool, has_second_skill: bool) -> void:
+	self.path = path
+	match path:
+		EvolutionPaths.WINGS:
+			has_double_jump = has_first_skill
+			has_dash = has_second_skill
+		EvolutionPaths.BRAIN:
+			has_slo_mo = has_first_skill
+			has_rewind = has_second_skill
+		EvolutionPaths.CLAWS:
+			has_climb = has_first_skill
+			has_destroy_obstacles = has_second_skill
+	evolution_updated.emit()
