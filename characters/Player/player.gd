3d9f4fb9df5deaaa0e545170722a5b8de26cfa74
scottmez <scottmesdjian10@gmail.com>
2024-08-16 22:50:37 -0700

    added limboai and dash

--- characters/Player/player.gd
+++ characters/Player/player.gd
@@ -5,8 +5,15 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY: float = -400.0 #
 @export var extra_jumps: int = 0
 @export var GLIDE_VELOCITY: float = 40.0
-@export var has_glide: bool = false ##has_glide and has_float should NOT both be true
+@export var has_glide: bool = false 
+@export var dash_duration: float = 0.25
+@export var dash_cooldown_duration: float = 1.0
+@export var DASH_SCALAR: float = 1.4
+var dash_cooldown_timer: float = 0.0
 var is_space_held: bool = false
+var dash_timer: float = 0.0
+var dash_direction: float = 0 #need dash direction so player's dash direction is fixed throughout the dash
+var direction: float = 0
 #climb velocity
 #double jump velocity, etc.
 
@@ -15,6 +22,11 @@ var is_space_held: bool = false
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var _remaining_jumps: int = extra_jumps
+var player_sm: LimboHSM
+
+func _ready():
+	initiate_state_machine() 
+	#technically only need this if we have dash rn, but we might use it for animations later
 
 
 func _process(delta):
@@ -45,8 +57,11 @@ func _physics_process(delta):
 	if is_space_held and has_glide and not is_on_floor() and velocity.y > 0: 
 		velocity.y = GLIDE_VELOCITY
 	
-	var direction = Input.get_axis("ui_left","ui_right")
+	direction = Input.get_axis("ui_left","ui_right")
 	velocity.x = SPEED * direction
+	if (player_sm.get_active_state().name == &"dash"):
+		velocity.x = dash_direction * SPEED * DASH_SCALAR
+		velocity.y = 0
 	
 	_state_record.store_record(get_time_record())
 	move_and_slide()
@@ -64,3 +79,54 @@ func set_from_time_record(record: Dictionary) -> void:
 
 func has_remaining_jumps() -> bool:
 	return _remaining_jumps > 0
+
+func initiate_state_machine() -> void:
+	player_sm = LimboHSM.new()
+	add_child(player_sm)
+	
+	#not dashing, not cooldown, can dash
+	var idle_state = LimboState.new().named("idle").call_on_enter(idle_start).call_on_update(idle_update)
+	var dash_state = LimboState.new().named("dash").call_on_enter(dash_start).call_on_update(dash_update)
+	var cooldown_state = LimboState.new().named("cooldown").call_on_enter(cooldown_start).call_on_update(cooldown_update)
+	
+	player_sm.add_child(idle_state)
+	player_sm.add_child(dash_state)
+	player_sm.add_child(cooldown_state)
+	
+	player_sm.initial_state = idle_state
+	
+	player_sm.add_transition(idle_state, dash_state, &"to_dash")
+	player_sm.add_transition(dash_state, cooldown_state, &"to_cooldown")
+	player_sm.add_transition(cooldown_state, idle_state, &"to_idle")
+	
+	player_sm.initialize(self)
+	player_sm.set_active(true)
+	
+	
+
+func idle_start():
+	print("idle_start")
+func idle_update(delta: float):
+	print("idle_update")
+	#dash pressed with directional input
+	if (Input.is_action_just_pressed("dash") and not is_equal_approx(direction, 0)): 
+		dash_direction = direction
+		player_sm.dispatch(&"to_dash")
+
+func dash_start():
+	print("dash_start")
+	dash_timer = dash_duration
+func dash_update(delta: float):
+	print("dash_update")
+	dash_timer -= delta
+	if (dash_timer <= 0):
+		player_sm.dispatch(&"to_cooldown")
+
+func cooldown_start():
+	print("cooldown_start")
+	dash_cooldown_timer = dash_cooldown_duration
+func cooldown_update(delta: float):
+	print("cooldown_update")
+	dash_cooldown_timer -= delta
+	if (dash_cooldown_timer <= 0):
+		player_sm.dispatch(&"to_idle")
