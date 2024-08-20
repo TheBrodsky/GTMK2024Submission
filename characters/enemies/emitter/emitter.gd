5f54c6d268cfbaa368e617028a9d34061d7438f2
TheBrodsky <stephensbrod@gmail.com>
2024-08-20 00:11:23 -0700

    fixed emitter bug

--- characters/enemies/emitter/emitter.gd
+++ characters/enemies/emitter/emitter.gd
@@ -5,6 +5,12 @@ extends Node2D
 @export var emission_velocity: float = 200
 
 var _time_to_emission: float = seconds_per_emission
+var _flip_v: bool = false
+
+
+func _ready() -> void:
+	if abs(rotation) > PI/2:
+		_flip_v = true
 
 
 func _process(delta: float) -> void:
@@ -19,5 +25,10 @@ func emit() -> void:
 	if "speed" in emission:
 		emission.speed = emission_velocity
 	emission.rotation = rotation
+	
+	if emission.animation_sprite != null:
+		emission.animation_sprite.flip_h = true
+		emission.animation_sprite.flip_v = _flip_v
+	
 	add_child(emission)
 	emission.global_position = global_position
